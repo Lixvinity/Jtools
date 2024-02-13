c9cd43c421040c57e866328b6780b2e4912b141e
James <131554210+Lixvinity@users.noreply.github.com>
2024-02-12 19:34:44 -0500

    Update ImageCompressor.py
    Color coded text and added support for webp

--- ImageCompressor.py
+++ ImageCompressor.py
@@ -3,19 +3,44 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 import time
+from webptools import dwebp
 
-DEFAULT_SAVE_PATH = "C:\\Users\\Master\\Desktop\\Tools\\convertedphotos\\Compressed"
+Custom_Path = False
+SAVE_PATH_FOLDER = os.path.expanduser("~\\Downloads")
+if Custom_Path == True:
+    SAVE_PATH_FOLDER = (r"C:\Users\Master\Desktop\Tools\convertedphotos\Compressed")
+
+
 DEFAULT_SCALE_PERCENTAGE = 100
+
+class Color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
 
 def open_file_dialog():
     root = tk.Tk()
     root.withdraw()  # Hide the main window
 
-    file_paths = filedialog.askopenfilenames(title="Select Images", filetypes=[("Image files", "*.jpg;*.png;*.ico;*.jpeg*")])
+    file_paths = filedialog.askopenfilenames(title="Select Images", filetypes=[("Image files", "*.jpg;*.png;*.ico;*.jpeg;*.webp")])
+    
     if not file_paths:
-        print("No files selected. Exiting.")
+        print(Color.RED + "No files selected. Exiting." + Color.END)
         exit()
+    
     return file_paths
+
+def convert_webp_to_png(webp_path):
+    output_path = os.path.splitext(webp_path)[0] + ".png"
+    dwebp(webp_path, output_path)
+    return output_path
 
 def scale_and_save_image(image_path, scale_percentage, compress_quality, file_type):
     try:
@@ -34,47 +59,49 @@ def scale_and_save_image(image_path, scale_percentage, compress_quality, file_ty
     resized_image = image.resize(scaled_size, Image.LANCZOS)
 
     if file_type.lower() in ['jpg', 'jpeg'] and resized_image.mode == 'RGBA':
-        # Convert RGBA to RGB by removing the alpha channel
         resized_image = resized_image.convert('RGB')
 
-
-    save_path = os.path.join(DEFAULT_SAVE_PATH, f"{original_name}_JTOOL_compressed.{file_type}")
+    save_path = os.path.join(SAVE_PATH_FOLDER, f"{original_name}Jtools_compressed.{file_type}")
 
     resized_image.save(save_path, optimize=True, quality=compress_quality)
     print(f"Image {image_path} saved to {save_path}")
 
 if __name__ == "__main__":
-    print(r"""
-       _            _                     _                 _____    _____   ___    _  _   
-      | |          | |                   | |               |_   _|  / ____| |__ \  | || |  
-      | |  ______  | |_    ___     ___   | |    ___          | |   | |         ) | | || |_ 
-  _   | | |______| | __|  / _ \   / _ \  | |   / __|         | |   | |        / /  |__   _|
- | |__| |          | |_  | (_) | | (_) | | |   \__ \  _     _| |_  | |____   / /_     | |  
-  \____/            \__|  \___/   \___/  |_|   |___/ ( )   |_____|  \_____| |____|    |_|  
-                                                     |/                                    
-                                                                                           """)
-    print("J-tools Image compressor/converter 2024")
-    print("Version 2.0 - supports Bulk transform and PNG, JPG/JPEG, ICO")
+    print(f"""{Color.BOLD}{Color.BLUE}
+          _            _                     _                 
+         | |          | |                   | |                
+         | |  ______  | |_    ___     ___   | |    ___         
+     _   | | |______| | __|  / _ \   / _ \  | |   / __| 
+    | |__| |          | |_  | (_) | | (_) | | |   \__ \ 
+     \____/            \__|  \___/   \___/  |_|   |___/
+                                                                
+    {Color.END}""")
+    print(f"{Color.YELLOW}J-tools Image compressor/converter 2024{Color.END}")
+    print(f"{Color.YELLOW}Version 2.1 - supports Bulk transform and PNG, JPG/JPEG, ICO{Color.END}")
+    print (f"{Color.YELLOW}Save folder is {SAVE_PATH_FOLDER}{Color.END}")
     time.sleep(1)
 
     image_paths = open_file_dialog()
-    file_extension = input("File extension: ").lower()
+    file_extension = input(f"{Color.GREEN}File extension: {Color.END}").lower()
 
-    if file_extension not in ['jpg', 'jpeg', 'png', 'ico']:
+    if file_extension not in ['jpg', 'jpeg', 'png', 'ico', 'webp']:
         print("Unsupported file extension. Exiting.")
         exit()
 
     compress_quality = 0
-    scale_percentage = input(f"Scale image by what percentage? (Default: {DEFAULT_SCALE_PERCENTAGE}): ") or DEFAULT_SCALE_PERCENTAGE
+    scale_percentage = input(f"{Color.CYAN}Scale image by what percentage? (Default: {DEFAULT_SCALE_PERCENTAGE}): {Color.END}") or DEFAULT_SCALE_PERCENTAGE
 
     try:
         scale_percentage = float(scale_percentage)
     except ValueError:
-        print("Invalid scale percentage. Using default value (100)")
+        print(f"{Color.RED}Invalid scale percentage. Using default value (100){Color.END}")
         scale_percentage = DEFAULT_SCALE_PERCENTAGE
 
     if file_extension in ["jpg", "jpeg"]:
-        compress_quality = int(input("Image quality: "))
+        compress_quality = int(input(f"{Color.CYAN}Image quality: {Color.END}"))
 
     for path in image_paths:
+        if file_extension == ".webp":
+            path = convert_webp_to_png(path)
+            file_extension = "png"
         scale_and_save_image(path, scale_percentage, compress_quality, file_extension)
